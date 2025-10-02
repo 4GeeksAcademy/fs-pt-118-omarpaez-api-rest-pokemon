0d5e91d87bd5c2a107acbc7393ba6a91c35c3bc9
Omar Páez <135053433+omarpaezdev@users.noreply.github.com>
2025-10-02 18:17:15 +0000

    feat: API REST Pokemon

--- src/app.py
+++ src/app.py
@@ -8,7 +8,10 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, User, Pokemon, Items, FavoritesPokemon, FavoritesItems
+from sqlalchemy import select
+from sqlalchemy.exc import IntegrityError
+
 #from models import Person
 
 app = Flask(__name__)
@@ -36,14 +39,112 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
+
+
+# ------------------------
+# Helpers
+# ------------------------
+def commit_and_serialize(obj):
+    db.session.add(obj)
+    db.session.commit()
+    return jsonify(obj.serialize()), 201
+
+
+# ------------------------
+# USERS
+# ------------------------
+
 @app.route('/user', methods=['GET'])
-def handle_hello():
+def get_users():
+    users = User.query.all()
+   
+    return jsonify([u.serialize() for u in users])
 
-    response_body = {
-        "msg": "Hello, this is your GET /user response "
-    }
+@app.route('/user/<int:id>', methods=["GET"])
+def get_user(id):
+    user = User.query.get_or_404(id)
+    return jsonify(user.serialize())
 
-    return jsonify(response_body), 200
+
+# ------------------------
+# POKEMONS
+# ------------------------
+@app.route('/pokemons', methods=['GET'])
+def get_pokemons():
+    pokemons = Pokemon.query.all()
+    return jsonify([p.serialize() for p in pokemons])
+
+@app.route('/pokemon/<name>', methods=["GET"])
+def get_pokemon(name):
+    query = select(Pokemon).where(Pokemon.name == name)
+    pokemon = db.session.execute(query).scalar_one()
+  
+    return jsonify(pokemon.serialize())
+
+
+
+# ------------------------
+# ITEMS
+# ------------------------
+
+@app.route('/items', methods=['GET'])
+def get_items():
+    items = Items.query.all()
+    return jsonify([i.serialize() for i in items])
+
+@app.route('/item/<name>', methods=["GET"])
+def get_item(name):
+    query = select(Items).where(Items.name == name)
+    item = db.session.execute(query).scalar_one()
+    
+    return jsonify(item.serialize())
+
+
+# ------------------------
+#  FAVORITESPOKEMONS
+# ------------------------
+
+## se agrega un pokemon a favoritos con el Id del usuario
+@app.route('/favorite/pokemon/<int:id>', methods=['POST'])
+def add_favorite_pokemon(id):
+    data = request.json
+    pokemonfavorite = FavoritesPokemon(user_id = id, pokemon_id = data["pokemon_id"])
+    try:
+        return commit_and_serialize(pokemonfavorite)
+    except IntegrityError:  ## captura el error en caso de querer agregar dos veces el mismo pokemon como favorito
+        db.session.rollback()
+        return jsonify({"error": "Error al agregar el Pokemon a favoritos"}), 400
+    
+
+##se obtiene los pokemons favoritos del usuario (id)
+@app.route('/user/<int:id>/favorites/pokemons', methods=["GET"])
+def get_favorites_pokemon(id):
+    query = select(FavoritesPokemon).where(FavoritesPokemon.user_id == id)
+    favorites = db.session.execute(query).scalars().all()
+    return jsonify([fav.pokemon.serialize() for fav in favorites])
+
+
+# ------------------------
+#  FAVORITESITEMS
+# ------------------------
+## se agrega un item a favoritos con el Id del usuario
+@app.route('/favorite/item/<int:id>', methods=['POST'])
+def add_favorite_item(id):
+    data = request.json
+    itemfavorite = FavoritesItems(user_id = id, item_id = data["item_id"])
+    try:
+       return commit_and_serialize(itemfavorite)
+    except IntegrityError:  ## captura el error en caso de querer agregar dos veces el mismo item como favorito
+        db.session.rollback()
+        return jsonify({"error": "Error al agregar el Item a favoritos"}), 400
+    
+##se obtiene los items favoritos del usuario (id)
+@app.route('/user/<int:id>/favorites/items', methods=["GET"])
+def get_favorites_items(id):
+    query = select(FavoritesItems).where(FavoritesItems.user_id == id)
+    favorites = db.session.execute(query).scalars().all()
+    return jsonify([fav.items.serialize() for fav in favorites])
+
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -1,19 +1,123 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import String, Boolean
-from sqlalchemy.orm import Mapped, mapped_column
-
+from sqlalchemy import String, Boolean, Date, DateTime, ForeignKey, UniqueConstraint
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from datetime import datetime, date
 db = SQLAlchemy()
 
 class User(db.Model):
+    __tablename__ ='user'
     id: Mapped[int] = mapped_column(primary_key=True)
+    username: Mapped[str] = mapped_column(String(120), nullable=False)
+    firstname: Mapped[str] = mapped_column(String(120), nullable=False)
+    lastname: Mapped[str] = mapped_column(String(120), nullable= False)
     email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
     password: Mapped[str] = mapped_column(nullable=False)
+    created_at: Mapped[datetime] = mapped_column(
+        DateTime, default=datetime.utcnow)
     is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
+    favoritespokemon: Mapped[list["FavoritesPokemon"]] = relationship(back_populates='user', uselist=True)
+    favoritesitems: Mapped[list["FavoritesItems"]] = relationship(back_populates='user', uselist=True)
 
 
     def serialize(self):
         return {
             "id": self.id,
+            "username": self.username,
             "email": self.email,
+            "favorites_items": [fav.serialize() for fav in self.favoritesitems]
+
             # do not serialize the password, its a security breach
         }
+
+
+class Pokemon(db.Model):
+    __tablename__ ='pokemon'
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(50), nullable=False)
+    url_img: Mapped[str] = mapped_column(String(150), nullable=False)
+    favoritespokemon: Mapped[list["FavoritesPokemon"]] = relationship(back_populates='pokemon', uselist=True)
+    types: Mapped[list["PokemonType"]] = relationship(back_populates='pokemon', uselist=True)
+
+    def serialize(self):
+        return{
+            "id": self.id,
+            "name": self.name,
+            "url_img": self.url_img
+        }
+
+class Type(db.Model):
+    __tablename__ ='type'
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(50), nullable=False)
+    pokemontype: Mapped["PokemonType"] = relationship(back_populates='type', uselist=False)
+
+class PokemonType(db.Model):
+    __tablename__ ='pokemontype'
+    id: Mapped[int] = mapped_column(primary_key=True)
+    pokemon_id: Mapped[int]= mapped_column(ForeignKey('pokemon.id'))
+    pokemon: Mapped["Pokemon"] = relationship(back_populates='types', uselist=False)
+    type_id: Mapped[int]= mapped_column(ForeignKey('type.id'))
+    type: Mapped["Type"] = relationship(back_populates='pokemontype', uselist=False)
+    
+
+    
+class Items(db.Model):
+    __tablename__ ='items'
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(50), nullable=False)
+    url_img: Mapped[str] = mapped_column(String(150), nullable=False)
+    favoritesitems: Mapped[list["FavoritesItems"]] = relationship(back_populates='items', uselist=True)
+    
+    def serialize(self):
+        return{
+            "id": self.id,
+            "name": self.name,
+            "url_img": self.url_img
+        }
+    
+class FavoritesItems(db.Model):
+    __tablename__ ='favoritesitems'
+    id: Mapped[int] = mapped_column(primary_key=True)
+    created_at: Mapped[datetime] = mapped_column(
+        DateTime, default=datetime.utcnow)
+    user_id: Mapped[int] = mapped_column(ForeignKey('user.id'))
+    user:Mapped["User"] = relationship(back_populates='favoritesitems', uselist=False)
+    item_id: Mapped[int] = mapped_column(ForeignKey('items.id'))
+    items:Mapped["Items"] = relationship(back_populates='favoritesitems')
+   
+    # Evitamos que se pueda agregar dos veces como favorito
+    __table_args__ = (UniqueConstraint('user_id', 'item_id', name='_user_item_uc'),)
+
+    def serialize(self):
+        return{
+            "id": self.id,
+            "user":{"username": self.user.username} if self.user else None,
+           # "items":{"id": self.items.id, "name": self.items.name}
+            "item": self.items.serialize() if self.items else None
+        }
+
+
+
+
+class FavoritesPokemon(db.Model):
+    __tablename__ ='favoritespokemon'
+    id: Mapped[int] = mapped_column(primary_key=True)
+    created_at: Mapped[datetime] = mapped_column(
+        DateTime, default=datetime.utcnow)
+    user_id: Mapped[int] = mapped_column(ForeignKey('user.id'))
+    user:Mapped["User"] = relationship(back_populates='favoritespokemon', uselist=False)
+    pokemon_id: Mapped[int] = mapped_column(ForeignKey('pokemon.id'))
+    pokemon:Mapped["Pokemon"] = relationship(back_populates='favoritespokemon', uselist=False)
+
+    # Evitamos que se pueda agregar dos veces como favorito
+    __table_args__ = (UniqueConstraint('user_id', 'pokemon_id', name='_user_pokemon_uc'),)
+
+    def serialize(self):
+        return{
+            "id": self.id,
+            "user":{"username": self.user.username} if self.user else None,
+            "pokemons":{"id": self.pokemon.id, "name": self.pokemon.name}
+        }
+
+
+
